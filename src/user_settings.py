47acfaf5aad9b781c7c8b6ce0b456913baf0b6cc
Irving Martinez <irvingmp6@hotmail.com>
2024-04-25 09:22:53 -0500

    Refactored User Settings; fixed unit tests

--- src/user_settings.py
+++ src/user_settings.py
@@ -2,17 +2,21 @@ import os
 from configparser import ConfigParser
 
 
-class ImportParserUserSettings:
-    def __init__(self, args):
-        self.args = args
-        self.conn = self.args.sqlite_db if hasattr(self.args, 'sqlite_db') else None
-        self.csv_file = self.args.csv_file
-        self.account_alias = self.args.account_alias
-        self.commit = self.args.commit
-        self.import_config = self.get_import_config() if hasattr(self.args, 'import_config') else None
+class UserSettings:
+    def __init__(self, cli_args):
+        self.cli_args = cli_args
+        self.conn = self.cli_args.sqlite_db if hasattr(self.cli_args, 'sqlite_db') else None
+
+class ImportParserUserSettings(UserSettings):
+    def __init__(self, cli_args):
+        super(ImportParserUserSettings, self).__init__(cli_args)
+        self.csv_file = self.cli_args.csv_file
+        self.account_alias = self.cli_args.account_alias
+        self.commit = self.cli_args.commit
+        self.import_config = self.get_import_config() if hasattr(self.cli_args, 'import_config') else None
 
     def get_import_config(self):
-        import_config_filepath = self.args.import_config
+        import_config_filepath = self.cli_args.import_config
         if not os.path.isfile(import_config_filepath):
             raise FileNotFoundError(f"Could not locate config file:\n{import_config_filepath}")
         cp = ConfigParser()
--- tests/test_user_settings.py
+++ tests/test_user_settings.py
@@ -10,41 +10,41 @@ class TestImportParserUserSettings(TestCase):
     @patch('src.user_settings.hasattr')
     def test__init__(self, hasattr_mock):
         hasattr_mock.side_effect = [True, False]
-        args = MagicMock()
-        args.sqlite_db = MagicMock()
-        args.csv_file = "/path/to/file.csv"
-        args.account_alias = "My Bank"
-        args.commit = False
+        cli_args = MagicMock()
+        cli_args.sqlite_db = MagicMock()
+        cli_args.csv_file = "/path/to/file.csv"
+        cli_args.account_alias = "My Bank"
+        cli_args.commit = False
     
-        user_settings = ImportParserUserSettings(args)
+        user_settings = ImportParserUserSettings(cli_args)
 
-        self.assertEqual(user_settings.conn, args.sqlite_db)
-        self.assertEqual(user_settings.csv_file, args.csv_file)
-        self.assertEqual(user_settings.account_alias, args.account_alias)
-        self.assertEqual(user_settings.commit, args.commit)
+        self.assertEqual(user_settings.conn, cli_args.sqlite_db)
+        self.assertEqual(user_settings.csv_file, cli_args.csv_file)
+        self.assertEqual(user_settings.account_alias, cli_args.account_alias)
+        self.assertEqual(user_settings.commit, cli_args.commit)
         self.assertEqual(user_settings.import_config, None)
 
     @patch('src.user_settings.os.path.isfile')
     @patch('src.user_settings.ConfigParser')
     def test__init__with_import_config(self, ConfigParser_mock, isfile_mock):
         isfile_mock.return_value = True
-        args = MagicMock()
-        args.csv_file = "/path/to/file.csv"
-        args.account_alias = "My Bank"
-        args.commit = False
+        cli_args = MagicMock()
+        cli_args.csv_file = "/path/to/file.csv"
+        cli_args.account_alias = "My Bank"
+        cli_args.commit = False
     
-        user_settings = ImportParserUserSettings(args)
+        user_settings = ImportParserUserSettings(cli_args)
 
-        self.assertEqual(user_settings.conn, args.sqlite_db)
-        self.assertEqual(user_settings.csv_file, args.csv_file)
-        self.assertEqual(user_settings.account_alias, args.account_alias)
-        self.assertEqual(user_settings.commit, args.commit)
+        self.assertEqual(user_settings.conn, cli_args.sqlite_db)
+        self.assertEqual(user_settings.csv_file, cli_args.csv_file)
+        self.assertEqual(user_settings.account_alias, cli_args.account_alias)
+        self.assertEqual(user_settings.commit, cli_args.commit)
         self.assertEqual(user_settings.import_config, ConfigParser_mock.return_value)
 
     @patch('src.user_settings.os.path.isfile')
     def test__init__import_config_file_not_found(self, isfile_mock):
         isfile_mock.return_value = False
-        args = MagicMock()
+        cli_args = MagicMock()
     
         with self.assertRaises(FileNotFoundError) as context:
-            ImportParserUserSettings(args)
+            ImportParserUserSettings(cli_args)
